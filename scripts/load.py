28d406e3b16a03a0854ee4a5ae69f07fcccbd065
Vestallin <lift_tosruksa@hotmail.com>
2023-06-19 17:57:14 +1000

    Added and optimized Ship data

--- scripts/load.py
+++ scripts/load.py
@@ -3,17 +3,34 @@ import csv
 
 
 def run():
-    with open('./ALStats.csv', encoding="utf-8") as file:
+    with open("./ALStatV2.csv", encoding="ISO-8859-1") as file:
         reader = csv.reader(file)
         next(reader)  # Advance past the header
 
         Ship.objects.all().delete()
 
         for row in reader:
-            row = [0 if x in ["??", ""]  else x for x in row]
-            film = Ship(ship_name=row[2], class_name=row[1], sid=row[0], hp=row[4],
-                        rarity=row[3], fp=row[5], trp=row[6], avi=row[7], aa=row[8],
-                        rld=row[9], eva=row[10], armor=row[11], spd=row[12], acc=row[13], lck=row[14],
-                        asw=row[15], oil=row[16], oxy=row[17], ammo=row[18], faction=row[19],
-                        )
+            row = [0 if x in ["??", ""] else x for x in row]
+            film = Ship(
+                sid=row[0],
+                ship_name=row[1],
+                rarity=row[2],
+                faction=row[3],
+                class_name=row[4],
+                lck=row[5],
+                armor=row[6],
+                spd=row[7],
+                hp=row[8],
+                fp=row[9],
+                aa=row[10],
+                trp=row[11],
+                eva=row[12],
+                avi=row[13],
+                oil=row[14],
+                rld=row[15],
+                asw=row[16],
+                oxy=row[17],
+                ammo=row[18],
+                acc=row[19],
+            )
             film.save()
